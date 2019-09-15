3d591cbb31f7eb45ef7d69a3ad0e5020cdd056a9
Dimitry Kh <dimitry@ethereum.org>
2019-09-15 16:03:19 +0300

    fix report of a missing genesis template config

--- retesteth/Options.cpp
+++ retesteth/Options.cpp
@@ -446,7 +446,7 @@ std::vector<ClientConfig> const& Options::DynamicOptions::getClientConfigs()
             {
                 fs::path configGenesisTemplatePath = genesisTemplatePath / (net + ".json");
                 ETH_FAIL_REQUIRE_MESSAGE(fs::exists(configGenesisTemplatePath),
-                    "template .json config for network '" + net + "' in " + clientName);
+                    "template .json config for network '" + net + "' in " + clientName + " not found in tests/Retesteth configs!");
                 cfg.addGenesisTemplate(net, configGenesisTemplatePath);
             }
             //*/ Load genesis templates
