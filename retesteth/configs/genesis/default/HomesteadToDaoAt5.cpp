4bd2af85916c9bf248beb63c20cfdc7141522bd6
Dimitry Kh <dimitry@ethereum.org>
2020-03-06 13:17:13 +0100

    fix daoTransition fork name

--- retesteth/configs/genesis/default/HomesteadToDaoAt5.cpp
+++ retesteth/configs/genesis/default/HomesteadToDaoAt5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string default_HomesteadToDaoAt5_config = R"({
     "params" : {
         "homesteadForkBlock" : "0x00",
-        "daoForkBlock" : "0x05"
+        "daoHardforkBlock" : "0x05"
     },
     "accounts" : {
     }
