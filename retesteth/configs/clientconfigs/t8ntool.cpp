31b5c8ed46922bd82ff57da27b21873f4d68172d
qbzzt <qbzzt1@gmail.com>
2021-06-09 08:13:49 -0500

    Modified the TR_FeeCapLessThanBlocks message
    It is different in the latest geth

--- retesteth/configs/clientconfigs/t8ntool.cpp
+++ retesteth/configs/clientconfigs/t8ntool.cpp
@@ -158,7 +158,7 @@ string const t8ntool_config = R"({
       "TR_IntrinsicGas" : "intrinsic gas too low:",
       "TR_NoFunds" : "insufficient funds for gas * price + value",
       "TR_NoFundsValue" : "insufficient funds for transfer",
-      "TR_FeeCapLessThanBlocks" : "fee cap less than block base fee",
+      "TR_FeeCapLessThanBlocks" : "max fee per gas less than block base fee",
       "TR_GasLimitReached" : "gas limit reached",
       "TR_NonceTooHigh" : "nonce too high",
       "TR_NonceTooLow" : "nonce too low",
