8bc372dac6179c3cd5aaedb951f237f3f47a2c28
Dimitry Kh <dimitry@ethereum.org>
2021-11-18 20:35:15 +0100

    support t8n receiptsRoot response

--- retesteth/testStructures/types/RPC/ToolResponse.cpp
+++ retesteth/testStructures/types/RPC/ToolResponse.cpp
@@ -11,7 +11,7 @@ ToolResponse::ToolResponse(DataObject const& _data)
     REQUIRE_JSONFIELDS(_data, "ToolResponse " + _data.getKey(),
         {{"stateRoot", {{DataType::String}, jsonField::Required}},
             {"txRoot", {{DataType::String}, jsonField::Required}},
-            {"receiptRoot", {{DataType::String}, jsonField::Required}},
+            {"receiptsRoot", {{DataType::String}, jsonField::Required}},
             {"logsHash", {{DataType::String}, jsonField::Required}},
             {"logsBloom", {{DataType::String}, jsonField::Required}},
             {"currentDifficulty", {{DataType::String}, jsonField::Required}},
@@ -21,7 +21,7 @@ ToolResponse::ToolResponse(DataObject const& _data)
 
     m_stateRoot = spFH32(new FH32(_data.atKey("stateRoot")));
     m_txRoot = spFH32(new FH32(_data.atKey("txRoot")));
-    m_receiptRoot = spFH32(new FH32(_data.atKey("receiptRoot")));
+    m_receiptsRoot = spFH32(new FH32(_data.atKey("receiptsRoot")));
     m_logsHash = spFH32(new FH32(_data.atKey("logsHash")));
     m_logsBloom = spFH256(new FH256(_data.atKey("logsBloom")));
     m_currentDifficulty = spVALUE(new VALUE(_data.atKey("currentDifficulty")));
