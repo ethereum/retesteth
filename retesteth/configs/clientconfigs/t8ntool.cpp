6691eb21d3c9833e6966b487935ed9d2a14d415d
Dimitry Kh <dimitry@ethereum.org>
2022-12-08 13:51:54 +0100

    fix yul.sh script more

--- retesteth/configs/clientconfigs/t8ntool.cpp
+++ retesteth/configs/clientconfigs/t8ntool.cpp
@@ -278,9 +278,10 @@ if [ -z $solc ]; then
    echo "0x"
 else
     out=$(solc --assemble $1 2>&1)
+    a=$(echo "$out" | grep "Binary representation:" -A 1 | tail -n1)
     case "$out" in
     *Error*) >&2 echo "yul.sh \"Yul compilation error: \"\n$out";;
-    *       )  a=$(echo $out | grep "Binary representation:" -A 1 | tail -n1) echo 0x$a;;
+    *       )  echo 0x$a;;
     esac
 fi
 )";
