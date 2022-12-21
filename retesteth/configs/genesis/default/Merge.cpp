ee6aeeb1a6a4b1cf563d30349ac718e6a3b4793e
Dimitry Kh <dimitry@ethereum.org>
2022-12-21 22:14:51 +0100

    rename geth t8n Merged into Merge

--- retesteth/configs/genesis/default/Merge.cpp
+++ retesteth/configs/genesis/default/Merge.cpp
@@ -24,7 +24,7 @@ const string default_Merge_config = R"({
 
 const string t8ntool_Merge_config = R"({
     "params" : {
-        "fork" : "Merged",
+        "fork" : "Merge",
         "terminalTotalDifficulty" : "0x00",
         "constantinopleForkBlock" : "0x00",
         "byzantiumForkBlock" : "0x00",
