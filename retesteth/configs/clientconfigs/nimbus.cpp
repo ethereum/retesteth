83939f744b4a9721686dfa80c170d5683e989ed8
Dimitry Kh <dimitry@ethereum.org>
2023-01-02 11:25:40 +0100

    nimbus t8n tool start.sh update

--- retesteth/configs/clientconfigs/nimbus.cpp
+++ retesteth/configs/clientconfigs/nimbus.cpp
@@ -240,16 +240,28 @@ if [ $1 = "-v" ]; then
     evm_nimbus -v
 else
     stateProvided=0
+    readErrorLog=0
+    errorLogFile=""
+    cmdArgs=""
     for index in ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ; do
         if [ $index = "--input.alloc" ]; then
             stateProvided=1
-            break
+        fi
+        if [ $readErrorLog -eq 1 ]; then
+            errorLogFile=$index
+            readErrorLog=0
+        fi
+        if [ $index = "--output.errorlog" ]; then
+            readErrorLog=1
+        fi
+        if [ $readErrorLog -eq 0 ]; then
+            cmdArgs=$cmdArgs" "$index
         fi
     done
     if [ $stateProvided -eq 1 ]; then
-        evm_nimbus ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} --verbosity 2
+        evm_nimbus $cmdArgs --verbosity 2 2> $errorLogFile
     else
-        evm_nimbus ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+        evm_nimbus $cmdArgs 2> $errorLogFile
     fi
 fi
 )";
