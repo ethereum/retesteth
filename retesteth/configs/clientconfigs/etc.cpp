54ae5725474b48ffa32f06f0f12428629b770c95
Dimitry Kh <dimitry@ethereum.org>
2023-02-09 13:23:32 +0100

    update etc start.sh

--- retesteth/configs/clientconfigs/etc.cpp
+++ retesteth/configs/clientconfigs/etc.cpp
@@ -229,16 +229,28 @@ if [ $1 = "-v" ]; then
     evmetc -v
 else
     stateProvided=0
-    for index in ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ; do
+    readErrorLog=0
+    errorLogFile=""
+    cmdArgs=""
+    for index in ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26}; do
         if [ $index = "--input.alloc" ]; then
             stateProvided=1
-            break
         fi
+        if [ $readErrorLog -eq 1 ]; then
+            errorLogFile=$index
+            readErrorLog=0
+            continue
+        fi
+        if [ $index = "--output.errorlog" ]; then
+            readErrorLog=1
+            continue
+        fi
+        cmdArgs=$cmdArgs" "$index
     done
     if [ $stateProvided -eq 1 ]; then
-        evmetc t8n ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} --verbosity 2
+        evmetc t8n $cmdArgs --verbosity 2 2> $errorLogFile
     else
-        evmetc t9n ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+        evmetc t9n $cmdArgs 2> $errorLogFile
     fi
 fi
 )";
