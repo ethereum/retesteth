61d2a8f33fd46b39ad38557c5d87aa771bd90f50
Dimitry Kh <dimitry@ethereum.org>
2023-07-27 08:30:50 +0200

    evmone pyspec exceptions

--- retesteth/configs/clientconfigs/evmone.cpp
+++ retesteth/configs/clientconfigs/evmone.cpp
@@ -55,6 +55,9 @@ string const evmone_config = R"({
     "fillerSkipForks" : [
     ],
     "exceptions" : {
+      "PYSPECS_EXCEPTIONS" : "",
+      "Transaction without funds" : "insufficient funds for gas * price + value",
+
       "AddressTooShort" : "input string too short for common.Address",
       "AddressTooLong" : "rlp: input string too long for common.Address, decoding into (types.Transaction)(types.LegacyTx).To",
       "NonceMax" : "nonce exceeds 2^64-1",
