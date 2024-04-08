faececb642b118789518122c4c3c7a984afeea09
Dimitry Kh <dimitry@ethereum.org>
2024-04-08 12:09:57 +0200

    fix pyspec exceptions

--- retesteth/configs/clientconfigs/evmone.cpp
+++ retesteth/configs/clientconfigs/evmone.cpp
@@ -74,7 +74,7 @@ string const evmone_config = R"({
       "TransactionException.INITCODE_SIZE_EXCEEDED" : "max initcode size exceeded",
       "BlockException.INCORRECT_EXCESS_BLOB_GAS" : "Error in field: excessBlobGas",
       "BlockException.INCORRECT_BLOB_GAS_USED" : "Error in field: blobGasUsed",
-      "BlockException.BLOB_GAS_USED_ABOVE_LIMIT|BlockException.INCORRECT_BLOB_GAS_USED" : "Error in field: blobGasUsed",
+      "BlockException.INCORRECT_BLOB_GAS_USED|BlockException.BLOB_GAS_USED_ABOVE_LIMIT" : "Error in field: blobGasUsed",
       "BlockException.INCORRECT_BLOCK_FORMAT" : "[retesteth]: Error importing raw rlp block: readBlockHeader(RLP): unknown block type!",
 
       "Transaction without funds" : "insufficient funds for gas * price + value",
